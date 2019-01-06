69486359751dbdf255c25188c4932f0f756b4576
dai.ishioka <dai.engr1107@gmail.com>
2019-01-06 06:49:33 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :users, only: [:index, :show, :new, :create]
 end
