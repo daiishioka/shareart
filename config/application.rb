ebaea5f0769b704b5fb2272a96bd3fd0efc815cb
dai.ishioka <dai.engr1107@gmail.com>
2019-01-14 12:33:20 +0000

    add for error

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
++ config.assets.initialize_on_precompile = false
+
 
 module Shareart
   class Application < Rails::Application
