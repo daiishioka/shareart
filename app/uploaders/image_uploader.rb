e1faac2290bd0d6e52904d9cd51892120e52217c
dai.ishioka <dai.engr1107@gmail.com>
2019-01-14 12:01:48 +0000

    Make Carrierwave work on Heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,6 +12,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -39,9 +43,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
--- config.ru
+++ config.ru
@@ -3,3 +3,7 @@
 require_relative 'config/environment'
 
 run Rails.application
+
+require ::File.expand_path('../config/environment',  __FILE__)
+use Rack::Static, :urls => ['/carrierwave'], :root => 'tmp' # adding this line
+run YourApplicationName::Application
